6bf44bd1dc4ae4d3be77f49f3a8ee2a20d4db11a
anisphia-palettia <euphyllia.tomodachi@gmail.com>
2025-07-27 20:51:00 +0700

    add genreate

--- src/modules/generate/generate.route.ts
+++ src/modules/generate/generate.route.ts
@@ -3,17 +3,20 @@ import {sendSuccess} from "@/lib/response";
 import {getMetadataFromPage} from "@/services/scraper";
 import {zodValidate} from "@/middleware";
 import {GenerateCreateBody, GenerateSchema} from "./generate.schema";
+import {HTTPException} from "hono/http-exception";
 
 const generateRoute = new Hono()
 
 generateRoute.post("", zodValidate("json", GenerateSchema.create), async (c) => {
     const {urlId} = c.req.valid("json") as GenerateCreateBody;
-    console.log(`https://timemark.com/s/${urlId}/8`);
     const response = await getMetadataFromPage(`https://timemark.com/s/${urlId}/8`);
 
-    console.log(response);
+    if (!response) {
+        throw new HTTPException(400, {message: "Data tidak berhasil di temukan atau url tidak valid"});
+    }
     return sendSuccess(c, {
-        message: "success generate data"
+        message: "success generate data",
+        data: response
     })
 })
 
